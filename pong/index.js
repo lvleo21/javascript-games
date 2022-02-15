1eed10637bcd9403260290b6b812a674a9269ebf
Leonardo Veras M. O. Lopes <verasleonardo210@gmail.com>
2022-02-15 11:37:32 -0300

    add collision with ball and players; add a change direction X/Y of ball; add score point to a game;

--- pong/index.js
+++ pong/index.js
@@ -5,7 +5,7 @@ class Main {
         this.container.fillStyle = "#8b8b8b";
         this.container.font = "20px Arial"
         this.elementsToDraw = [];
-
+        this.collision;
         this.player1;
         this.player2;
         this.ball;
@@ -16,34 +16,63 @@ class Main {
     start() {
         this.createElemests();
         this.draw();
-        this.ball.startMoving(0, this.canvas.width);
-        this.player1.startMoving(0, this.canvas.height)
+        this.verifyGameLoop();
+        this.ball.startMoving(this.collision);
+        this.player1.startMoving(0, this.canvas.height, false)
+        this.player2.startMoving(0, this.canvas.height, true)
 
 
         document.addEventListener("keydown", (event) => {
-            if (event.key === "ArrowUp"){
+            if (event.key === "ArrowUp") {
                 this.player1.changeSteps(-8);
-            } else if (event.key === "ArrowDown"){
+            } else if (event.key === "ArrowDown") {
                 this.player1.changeSteps(8);
             }
 
         })
 
         document.addEventListener("keyup", (event) => {
-            if (event.key === "ArrowUp"){
+            if (event.key === "ArrowUp") {
                 this.player1.changeSteps(0);
-            } else if (event.key === "ArrowDown"){
+            } else if (event.key === "ArrowDown") {
                 this.player1.changeSteps(0);
             }
         })
     }
 
+    restart() {
+        this.player1.posY = 125;
+        this.player2.posY = 125;
+        this.ball.posX = 400 - 12.5;
+        this.ball.posY = 200 - 12.5;
+    }
+
+
+    verifyGameLoop = () => {
+        setInterval(() => {
+            switch (this.collision.hasPointCollision()) {
+                case 1:
+                    this.player1.score.incrementValue();
+                    this.ball.steps = -8
+                    this.restart();
+                    break;
+                case 2:
+                    this.player2.score.incrementValue();
+                    this.ball.steps = 8
+                    this.restart();
+                    break;
+            }
+        }, 20)
+    }
+
+
     createElemests() {
-        const scorePlayer1 = new Score("Score 1: ", 40, 50);
+        const scorePlayer1 = new Score("P1: ", 40, 50);
         this.player1 = new Player(40, 125, 25, 100, scorePlayer1);
-        const scorePlayer2 = new Score("Score 2: ", this.canvas.width - 130, 50);
+        const scorePlayer2 = new Score("P2: ", this.canvas.width - 130, 50);
         this.player2 = new Player(735, 125, 25, 100, scorePlayer2);
         this.ball = new Ball(400 - 12.5, 200 - 12.5, 25, 25);
+        this.collision = new Collision(this.ball, this.player1, this.player2, 0, this.canvas.width, this.canvas.height);
 
         this.elementsToDraw.push(
             this.player1,
@@ -79,7 +108,7 @@ class Player extends Object {
     constructor(posX, posY, width, height, score) {
         super(posX, posY, width, height);
         this.score = score;
-        this.steps = 0; 
+        this.steps = 0;
     }
 
     draw(container) {
@@ -87,19 +116,34 @@ class Player extends Object {
         this.score.draw(container);
     }
 
-    changeSteps(steps){
-        this.steps=steps;
+    changeSteps(steps) {
+        this.steps = steps;
     }
 
-    startMoving(minWidth, maxWidth) {
-        setInterval(() => {
-            //TODO: Refactor this conditions, has a bug.;
-            let canMoveDown = this.steps === 8 && ((this.posY + this.height) <= maxWidth);
-            let canMoveUp = this.steps === -8 && (this.posY  >= minWidth);
+    movingAuto(minHeight, maxHeight) {
+        if (this.posY > maxHeight - this.height || this.steps === 0) {
+            this.changeSteps(-8);
+        } else if (this.posY < 0) {
+            this.changeSteps(8);
+        }
+    }
 
-            if (canMoveDown || canMoveUp){
-                this.posY+=this.steps;  
+    startMoving(minHeight, maxHeight, auto) {
+        setInterval(() => {
+
+            if (auto) {
+                this.movingAuto(minHeight, maxHeight)
             }
+
+            if (this.posY < minHeight) {
+                this.posY = 0;
+            } else if (this.posY + this.height > maxHeight) {
+                this.posY = maxHeight - this.height;
+            }
+
+            this.posY += this.steps;
+
+
         }, 10)
     }
 }
@@ -108,17 +152,27 @@ class Ball extends Object {
     constructor(posX, posY, width, height) {
         super(posX, posY, width, height)
         this.steps = 8
+        this.stepsY = -2;
     }
 
-    startMoving(minWidth, maxWidth) {  //!L <- Left; R -> Right;
+    startMoving(collision) {  //!L <- Left; R -> Right;
         setInterval(() => {
-            this.posX += this.steps;
-
-            if (this.posX > maxWidth - this.width) {
-                this.steps = -8;
-            } else if (this.posX < minWidth) {
-                this.steps = 8;
+            switch (collision.hasBallCollision()) {
+                case 1:
+                    this.steps = -8;
+                    break;
+                case 2:
+                    this.steps = 8;
+                    break;
+                case 3:
+                    this.stepsY = 2;
+                    break;
+                case 4:
+                    this.stepsY = -2;
+                    break;
             }
+            this.posX += this.steps;
+            this.posY += this.stepsY;
         }, 10)
     }
 }
@@ -137,6 +191,52 @@ class Score {
 
     incrementValue() {
         this.value += 1;
+    }
+
+    resetValue() {
+        this.value = 0;
+    }
+
+}
+
+
+class Collision {
+
+    constructor(ball, player1, player2, canvaMinWidth, canvaMaxWidth, canvaHeight) {
+        this.ball = ball;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.canvaMinWidth = canvaMinWidth;
+        this.canvaMaxWidth = canvaMaxWidth;
+        this.canvaHeight = canvaHeight;
+    }
+
+    hasPointCollision() {
+        if (this.ball.posX <= this.canvaMinWidth) {
+            return 2 // Ponto do player 2
+        } else if (this.ball.posX + this.ball.width >= this.canvaMaxWidth) {
+            return 1 // Ponto do ṕlayer 1
+        }
+    }
+
+    hasBallCollision() {
+
+        if (this.ball.posY + this.ball.height >= this.player2.posY &&
+            this.ball.posY <= this.player2.posY + this.player2.height &&
+            this.ball.posX >= this.player2.posX - this.player2.width &&
+            this.ball.posX >= this.player2.posX - this.player2.width) {
+            return 1; // 1 = Colisão a direita
+        } else if (
+            this.ball.posY + this.ball.height >= this.player1.posY &&
+            this.ball.posY <= this.player1.posY + this.player1.height &&
+            this.ball.posX <= this.player1.posX + this.player1.width
+        ) {
+            return 2; // 2 = Colisão a esquerda
+        } else if (this.ball.posY <= 0) {
+            return 3; // 3 = Colisão na parte de cima
+        } else if (this.ball.posY + this.ball.height >= this.canvaHeight) {
+            return 4 // 4 = Colisão na parte de baixo
+        }
     }
 
 }
