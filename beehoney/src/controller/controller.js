989e090c6ed3ee35945c2446c4aca93748af86d6
Leonardo Veras M. O. Lopes <verasleonardo210@gmail.com>
2022-02-16 13:20:38 -0300

    add flower and spider collision, add point counter, add life decrement

--- beehoney/src/controller/controller.js
+++ beehoney/src/controller/controller.js
@@ -2,22 +2,33 @@ class Controller {
     constructor(canvas) {
         this.view = new View(canvas);
         this.arrayElements = []
-
-        this.createElements();
-        this.start();
-
         this.bee;
         this.spider;
         this.background;
+        this.flower;
+        this.collision;
 
+        this.createElements();
+        this.start();
         this.addListener();
     }
 
     createElements() {
         this.background = new Background(0, 0, 500, 900, "assets/bg.png");
-        this.bee = new Bee(0, 0, 70, 58, "assets/bee1.png");
+        this.bee = new Bee(210, 800, 70, 58, "assets/bee1.png");
         this.spider = new Spider(0, 100, 70, 58, "assets/spider1.png");
-        this.arrayElements.push(this.background, this.bee, this.spider);
+        this.flower = new Flower(50, 400, 48, 48, "assets/florwer1.png");
+
+        this.collision = new Collider(this.bee, this.spider, this.flower);
+
+        this.arrayElements.push(
+            this.background,
+            this.bee,
+            this.bee.points,
+            this.bee.lifes,
+            this.spider,
+            this.flower,
+        );
     }
 
     addListener() {
@@ -26,11 +37,23 @@ class Controller {
     }
 
     start() {
+        //* Start Game Looping
         setInterval(() => {
             this.background.move(this.view.canvas.height);
             this.bee.move();
-            this.spider.move(this.view.canvas.height, this.view.canvas.width);
+            this.spider.randomMove(this.view.canvas.height, this.view.canvas.width);
+            this.flower.randomMove(this.view.canvas.height, this.view.canvas.width);
             this.view.draw(this.arrayElements);
+
+            this.collision.verifyCollision();
+
         }, 10)
+
+        //* Start Animation
+        setInterval(() => {
+            this.bee.animation();
+            this.spider.animation();
+            this.flower.animation();
+        }, 150)
     }
 }
--- beehoney/src/model/model.js
+++ beehoney/src/model/model.js
@@ -1,23 +1,52 @@
 class Sprit {
-    constructor(posX, posY, width, height, urlSprit) {
+    constructor(posX, posY, width, height, urlSprit, type, spritLimit) {
         this.posX = posX;
         this.posY = posY;
         this.width = width;
         this.height = height;
         this.sprit = BuildSprit.build(urlSprit);
-
+        this.type = type;
+        this.spritLimit = spritLimit;
+        this.spritFrame = 1;
     }
 
     draw(context) {
         context.drawImage(this.sprit, this.posX, this.posY, this.width, this.height);
     }
+
+    animation() {
+        this.sprit.src = `assets/${this.type}${this.spritFrame}.png`;
+
+        if (this.spritFrame === this.spritLimit) {
+            this.spritFrame = 1;
+        } else {
+            this.spritFrame++;
+        }
+    }
+
+    randomMove(maxHeight, maxWidth) {
+        this.posY += 5
+
+        if (this.posY >= maxHeight) {
+            this.posY = -50;
+            this.posX = Math.random() * ((maxWidth - this.width) - 0) + 0;
+        }
+    }
+
+    respaw() {
+        this.posY = -200;
+        this.posX = Math.random() * ((500 - this.width) - 0) + 0;
+    }
 }
 
 class Bee extends Sprit {
     constructor(posX, posY, width, height, urlSprit) {
-        super(posX, posY, width, height, urlSprit);
+        super(posX, posY, width, height, urlSprit, "bee", 4);
         this.stepsX = 0;
         this.stepsY = 0;
+        this.points = new Text(0, "40px Arial", 240, 100, "black");
+        this.lifes = new Text(3, "40px Arial", 40, 100, "red");
+
     }
 
     eventKeyDown(key) {
@@ -34,62 +63,142 @@ class Bee extends Sprit {
             case "ArrowDown":
                 this.stepsY = 8;
                 break
-
         }
     }
 
-    eventKeyUp(key){
-        if (key === "ArrowUp" || key === "ArrowDown"){
+    eventKeyUp(key) {
+        if (key === "ArrowUp" || key === "ArrowDown") {
             this.stepsY = 0;
-        } else if(key === "ArrowLeft" || key === "ArrowRight"){
+        } else if (key === "ArrowLeft" || key === "ArrowRight") {
             this.stepsX = 0;
         }
     }
 
-    move(){
-        this.posX+=this.stepsX;
-        this.posY+=this.stepsY;
+    move() {
+        this.posX += this.stepsX;
+        this.posY += this.stepsY;
+    }
+
+    decrementLife() {
+        this.lifes.setText(this.lifes.text - 1);
+    }
+
+    incrementPoints() {
+        this.points.setText(this.points.text + 1);
+    }
+
+    draw(context) {
+        context.drawImage(this.sprit, this.posX, this.posY, this.width, this.height);
     }
 }
 
 class Spider extends Sprit {
     constructor(posX, posY, width, height, urlSprit) {
-        super(posX, posY, width, height, urlSprit);
-    }
-
-    move(maxHeight, maxWidth){
-        this.posY+=10
-
-        if(this.posY >= maxHeight){
-            this.posY = -50;
-            this.posX = Math.random() * ((maxWidth - this.width) - 0) + 0;
-        }
+        super(posX, posY, width, height, urlSprit, "spider", 4);
     }
 }
 
-class Background extends Sprit{
+class Flower extends Sprit {
+    constructor(posX, posY, width, height, urlSprit) {
+        super(posX, posY, width, height, urlSprit, "florwer", 2);
+    }
+}
+
+class Background extends Sprit {
     constructor(posX, posY, width, height, urlSprit) {
         super(posX, posY, width, height, urlSprit);
     }
 
-    move(maxHeight){
-        this.posY+=5;
+    move(maxHeight) {
+        this.posY += 5;
 
-        if (this.posY === maxHeight){
+        if (this.posY === maxHeight) {
             this.posY = 0;
         }
     }
 
-    draw(context){
+    draw(context) {
         context.drawImage(this.sprit, this.posX, this.posY, this.width, this.height);
         context.drawImage(this.sprit, this.posX, this.posY - this.height, this.width, this.height);
     }
 }
 
-class BuildSprit{
-    static build(urlSprit){
+class BuildSprit {
+    static build(urlSprit) {
         let image = new Image();
         image.src = urlSprit;
         return image;
+    }
+}
+
+class Text {
+    constructor(text, fontStyle, posX, posY, color) {
+        this.text = text;
+        this.fontStyle = fontStyle;
+        this.posX = posX;
+        this.posY = posY;
+        this.color = color;
+    }
+
+    draw(context) {
+        context.font = this.fontStyle;
+        context.fillStyle = this.color;
+        context.fillText(this.text, this.posX, this.posY);
+    }
+
+    setText(text) {
+        this.text = text;
+    }
+
+    setFontStyle(fontStyle) {
+        this.fontStyle = fontStyle;
+    }
+
+    setPosX(posX) {
+        this.posX = posX;
+    }
+
+    setPosY(posY) {
+        this.posY = posY;
+    }
+
+    setColor(color) {
+        this.color = color;
+    }
+}
+
+class Collider {
+
+    constructor(bee, spider, flower) {
+        this.bee = bee;
+        this.spider = spider;
+        this.flower = flower;
+    }
+
+
+    verifyCollision() {
+
+        // Spider collision
+        if (this.hasCollision(this.bee, this.spider)) {
+            this.spider.respaw();
+            this.bee.decrementLife();
+        }
+        //flower collision
+        if (this.hasCollision(this.bee, this.flower)) {
+            this.flower.respaw();
+            this.bee.incrementPoints()
+        }
+
+    }
+
+    hasCollision(firstElement, secondElement) {
+        if (firstElement.posX < secondElement.posX + secondElement.width &&
+            firstElement.posX + firstElement.width > secondElement.posX &&
+            firstElement.posY < secondElement.posY + secondElement.height &&
+            firstElement.posY + firstElement.height > secondElement.posY
+        ) {
+            return true;
+        }
+        return false;
     }
 }
