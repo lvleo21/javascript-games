c2ebd7058381835ffd19ad5594c3ebd9c772e04c
Leonardo Veras M. O. Lopes <verasleonardo210@gmail.com>
2022-02-17 08:33:53 -0300

    add a main page; add nodemon to restart server; add routers to another games;

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require("express")
-const app = express()
-
-// use the express-static middleware
-app.use(express.static("beehoney"))
+const express = require("express");
+const app = express();
+const path = require("path")
 
 // define the first route
-// app.get("/", function (req, res) {
-//     res.send("<h1>Hello World!</h1>")
-// })
+app.get("/beehoney", function (req, res) {
+    res.sendFile(path.join(__dirname, 'beehoney/index.html'));
+})
+
+app.get("/pong", function (req, res) {
+    res.sendFile(path.join(__dirname, 'pong/index.html'));
+})
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, 'main.html'));
+})
+
+// use the express-static middleware
+app.use(express.static("beehoney"));
+app.use(express.static("pong"));
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
